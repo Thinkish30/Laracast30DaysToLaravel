121c64a5c03af792ae72ff2d39ed0f8bed639eb6
Vinod30 <91652066+Thinkish30@users.noreply.github.com>
2024-12-02 19:02:01 +0530

    commit-2

--- Day3/example-app/resources/views/Components/layout.blade.php
+++ Day3/example-app/resources/views/Components/layout.blade.php
@@ -12,6 +12,13 @@
 
     </head>
     <body>
-        <h1>About</h1>
+
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </nav>
+
+        {{ $slot }}
     </body>
 </html>
